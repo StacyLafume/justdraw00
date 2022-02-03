3ac30af8869bf8c30ddf147339422d63de05925c
Stacy Lafume <stacylafume123@gmail.com>
2022-02-03 17:38:42 -0500

    removed dep bcrypt

--- .history/app/models/user_20201206064143.js
+++ .history/app/models/user_20201206064143.js
@@ -1,6 +1,6 @@
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt   = require('ptjs');
+var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
