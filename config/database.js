640ba5c5eca56428b8f0133abd3536524ec0936a
Stacy Lafume <stacylafume123@gmail.com>
2025-06-09 23:10:07 -0400

    updated Mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
-
-
 module.exports = {
-
-    'url' : `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0-x3ldo.mongodb.net/justdraw?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': process.env.DB_NAME
-
-};
+    url: `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}` +
+         `@cluster0.xuwmnvb.mongodb.net/${process.env.DB_NAME}` +
+         `?retryWrites=true&w=majority&appName=Cluster0`,
+    dbName: process.env.DB_NAME
+  };
